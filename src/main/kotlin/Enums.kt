01f73529970bc2c8f0a0c0d29f8d3bc0592ab714
Chris James <qui666@gmail.com>
2017-07-26 16:24:59 +0100

    different example of when using setOf

--- src/main/kotlin/Enums.kt
+++ src/main/kotlin/Enums.kt
@@ -3,7 +3,7 @@
 This all feels very similar to sealed classes in scala, lets you write nice ADTs which is vital for FP
  */
 enum class ProgrammingLanguage {
-    Java, Rust, Kotlin, Scala;
+    Java, Rust, Kotlin, Scala, CSharp, Go;
 }
 
 /*
@@ -12,6 +12,13 @@ This is actually better than scala in that it doesnt compile if you dont cover a
 In Scala this is just a warning (at least in 2.10)
  */
 fun IsFlavourOfTheMonth(pl: ProgrammingLanguage): Boolean = when(pl) {
-    ProgrammingLanguage.Java, ProgrammingLanguage.Scala -> false
-    ProgrammingLanguage.Kotlin, ProgrammingLanguage.Rust -> true
+    ProgrammingLanguage.Java, ProgrammingLanguage.Scala, ProgrammingLanguage.CSharp -> false
+    ProgrammingLanguage.Kotlin, ProgrammingLanguage.Rust, ProgrammingLanguage.Go -> true
+}
+
+fun LabelAProgrammer(pl1: ProgrammingLanguage, pl2: ProgrammingLanguage): String {
+    return when(setOf(pl1, pl2)) {
+        setOf(ProgrammingLanguage.Scala, ProgrammingLanguage.Go) -> "Enlightened master"
+        else -> "Blub"
+    }
 }
--- src/test/kotlin/EnumsTest.kt
+++ src/test/kotlin/EnumsTest.kt
@@ -1,3 +1,5 @@
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
 import org.junit.Test
 
 class EnumsTest {
@@ -6,5 +8,11 @@ class EnumsTest {
     fun `identify FOM programming languages`() {
         assert(IsFlavourOfTheMonth(ProgrammingLanguage.Rust))
         assert(IsFlavourOfTheMonth(ProgrammingLanguage.Kotlin))
+    }
+
+    @Test
+    fun `identify programming masters based on pl knowledge`() {
+        assertThat("Enlightened master", equalTo(LabelAProgrammer(ProgrammingLanguage.Scala, ProgrammingLanguage.Go)))
+        assertThat("Blub", equalTo(LabelAProgrammer(ProgrammingLanguage.CSharp, ProgrammingLanguage.Java)))
     }
 }
