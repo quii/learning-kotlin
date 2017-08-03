e7c6f1f3c72945edff810ce7bf1ba8afc24f8f21
Chris James <qui666@gmail.com>
2017-08-03 16:24:01 +0100

    example of passing a top level function to map

--- src/main/kotlin/Collections.kt
+++ src/main/kotlin/Collections.kt
@@ -6,8 +6,10 @@ val people = hashMapOf(
 
 val scores = setOf(1, 2, 10, 5)
 
+fun pooAdder(x: String) = x + "poo"
+
 fun addPooToAll(vararg stuff: String): List<String> {
-    return stuff.map { x -> x + "poo" }
+    return stuff.map(::pooAdder)
 }
 
 fun doesPooExist(words: List<String>): Boolean {
